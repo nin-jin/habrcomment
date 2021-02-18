4cb7d8bd2a8155c3efbfe64fc8ff47d666e0fee1
nin-jin <nin-jin@users.noreply.github.com>
2021-02-18 07:16:17 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.test.js
+++ node.test.js
@@ -7657,8 +7657,7 @@ var $;
     (function ($$) {
         class $mol_select extends $.$mol_select {
             filter_pattern(next) {
-                if (!this.focused())
-                    return '';
+                this.focused();
                 return next || '';
             }
             open() {
