2bc2f4f6653f8073e19ac7a2c7a89bb34145176b
nin-jin <nin-jin@users.noreply.github.com>
2021-08-31 07:06:42 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.test.js
+++ node.test.js
@@ -5303,7 +5303,7 @@ var $;
         '@': {
             mol_link_current: {
                 'true': {
-                    color: $.$mol_theme.text,
+                    color: $.$mol_theme.focus,
                     textShadow: '0 0',
                 }
             }
